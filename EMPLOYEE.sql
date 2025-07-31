901955b2e6f7361f5e4bfcd69e8deaef0b94054f
SUNDHARAM S <sundharamsundar822@gmail.com>
2025-07-31 15:22:44 +0530

    Add files via upload

---
+++ EMPLOYEE.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE emp;
+use emp;
+CREATE TABLE EMP(
+emp_id INT PRIMARY KEY,
+name VARCHAR(20) NOT NULL,
+dep VARCHAR(30)NOT NULL,
+exp VARCHAR(20)NOT NULL);
+SELECT * FROM EMP;
+INSERT INTO EMP(emp_id,name,dep,exp) VALUES(101,'sana','sales',3);
+INSERT INTO EMP(emp_id,name,dep,exp) VALUES(120,'ram','tester',3);
+INSERT INTO EMP(emp_id,name,dep,exp) VALUES(130,'somu','sales',3);
+INSERT INTO EMP(emp_id,name,dep,exp) VALUES(140,'nilla','tester',3); 
+INSERT INTO EMP(emp_id,name,dep,exp) VALUES(150,'pallavi','tester',3);
+UPDATE EMP SET name ='ruku' WHERE  emp_id=120;
+
+
+
+
