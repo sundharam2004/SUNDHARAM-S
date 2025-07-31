a7e008c2d6096c0461e7ec32ba2b39ea896e8292
SUNDHARAM S <sundharamsundar822@gmail.com>
2025-07-31 15:34:52 +0530

    Update EMPLOYEE.sql

--- EMPLOYEE.sql
+++ EMPLOYEE.sql
@@ -11,7 +11,17 @@ INSERT INTO EMP(emp_id,name,dep,exp) VALUES(120,'ram','tester',3);
 INSERT INTO EMP(emp_id,name,dep,exp) VALUES(130,'somu','sales',3);
 INSERT INTO EMP(emp_id,name,dep,exp) VALUES(140,'nilla','tester',3); 
 INSERT INTO EMP(emp_id,name,dep,exp) VALUES(150,'pallavi','tester',3);
-UPDATE EMP SET name ='ruku' WHERE  emp_id=120;
+UPDATE EMP SET salary ='2000' WHERE  emp_id=120;
+UPDATE EMP SET salary ='2000' WHERE  emp_id=130;
+UPDATE EMP SET salary ='2000' WHERE  emp_id=140;
+UPDATE EMP SET salary ='2000' WHERE  emp_id=150;
+ALTER TABLE EMP
+ADD salary INT;
+
+
+
+
+
 
 
 
